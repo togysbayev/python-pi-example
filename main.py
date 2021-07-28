8dc2cf6c6d3a7b791e65ef6f8876056932f3021c
togysbayev <togysbayev.bekarys@gmail.com>
2021-07-28 11:42:46 +0600

    Change to main

--- main.py
+++ main.py
@@ -3,8 +3,10 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
+def main(a):
+    print(a)
 
-print("Hello")
+main("Hello")
 
 
 # Press the green button in the gutter to run the script.
